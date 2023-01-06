bb39a5b4e1a0e5696845f1488d277912fead3b36
Joakim Holm <joakimholm@protonmail.com>
2023-01-06 14:43:56 +0100

    Add marksman

--- default.nix
+++ default.nix
@@ -3,5 +3,6 @@
 {
   AAXtoMP3 = pkgs.callPackage ./packages/aaxtomp3.nix {};
   audible-cli = pkgs.callPackage ./packages/audible-cli.nix {};
+  marksman = pkgs.callPackage ./packages/marksman/default.nix {};
   pack-it = pkgs.callPackage ./packages/pack-it.nix {};
 }
